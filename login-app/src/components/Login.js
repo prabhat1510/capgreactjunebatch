2b30a6a365bcaffaf2b6b801a2c8544f24ac2e4f
prabhat1510 <prabhatc@gmail.com>
2022-06-09 10:11:30 +0530

    added basic login form

---
+++ login-app/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function Login(props){
+    return (
+        <div>
+            <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+</div>
+
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+  <span class="input-group-text" id="basic-addon2">@example.com</span>
+</div>
+        </div>
+    );
+}
+
+export default Login;
