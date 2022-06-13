3017ef706879c1ab3bebe6ec4166490f92722250
prabhat1510 <prabhatc@gmail.com>
2022-06-13 14:13:15 +0530

    Corrected the path

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
         <div className="container mt-3">
           <Switch>
             <Route exact path={"/tutorials"} component={TutorialsList} />
-            <Route exact path="add" component={AddTutorial} />
+            <Route exact path="/add" component={AddTutorial} />
             <Route path="/tutorials/:id" component={TutorialComponent} />
           </Switch>
         </div>
