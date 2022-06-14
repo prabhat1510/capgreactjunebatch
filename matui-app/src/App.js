edcb0ddb05144e2977a80dbecd341e73eb90ff39
prabhat1510 <prabhatc@gmail.com>
2022-06-14 12:14:30 +0530

    added material ui example

---
+++ matui-app/src/App.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip'
+import Button from '@mui/material/Button';
+/**
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}*/
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+
+
+export default App;
